b158b32d11648184a6395047ea5e0bccb4197a6d
Raimer Thompson <rthomp22@gmail.com>
2025-01-02 07:01:56 -0500

    cxs

--- js/app.js
+++ js/app.js
@@ -6,3 +6,18 @@ faqs.forEach(faq =>{
   })
 })
 
+let indexvalue = 1;
+showImg(indexvalue);
+function dot(e){showImg(indexvalue = e);}
+function showImg(e){
+  let i;
+  const img = document.querySelectorAll('.phase')
+  const sliderDot = document.querySelectorAll('.phase__dots span')
+  if (e > img.length){indexvalue = 1}
+  if (e < 1){indexvalue = img.length}
+  for (i = 0; i < img.length; i++){
+    img[i].style.display = "none";
+  }
+  img[indexvalue - 1].style.display = "flex";
+
+}
---
+++ js/contact.js
@@ -0,0 +1,32 @@
+const contactForm = document.getElementById('contact-form'),
+contactMessage = document.getElementById('contact-message')
+
+
+const sendEmail = (e) =>{
+  e.preventDefault()
+
+   // serviceID - templateID - #form - publicKey 
+  emailjs.sendForm('service_kmv55swsw9q', 'template_xnu3zswswqy', '#contact-form', 'IM4aNjqZUrkU_qswI4S')
+    .then(() =>{
+  //show sent message 
+    contactMessage.textContent = 'Thanks for Submitting!'
+
+  //remove message after 2 seconds
+  setTimeout(() => {
+    contactMessage.textContent = ''
+  }, 2000);
+
+  //clear input fields
+  contactForm.reset()
+  }, () =>{
+    //show error message
+    contactMessage.textContent = 'Sorry, message was not recieved ❌'
+  })
+}
+
+contactForm.addEventListener('submit', sendEmail);
+
+
+
+// Vs4d0AsUtYr3Zbvh_
+
